48766a0c9f45023ecc97614d5c7ee2c3645674f1
John Dykes <98johndykes@gmail.com>
2024-02-03 21:45:24 -0500

    nice animations when deleting flashcards

--- frontend/src/components/flashcards/FlashcardList.tsx
+++ frontend/src/components/flashcards/FlashcardList.tsx
@@ -1,16 +1,12 @@
-'use server';
-
 import { fetchFlashcardsJSON } from '../../services/api';
-import FlashcardCard from './FlashcardCard';
+import FlashcardListClient from './FlashcardList.client';
 
 export default async function FlashcardList() {
   const flashcards = await fetchFlashcardsJSON();
 
   return (
     <div className='flex flex-col gap-5 w-4xl'>
-      {flashcards.map((flashcard) => (
-        <FlashcardCard key={flashcard.id} flashcard={flashcard} />
-      ))}
+      <FlashcardListClient flashcards={flashcards}></FlashcardListClient>
     </div>
   );
 }
